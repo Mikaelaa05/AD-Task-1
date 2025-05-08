a25da53c56cc1e49aa0b864f498bd3f90a49eee0
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-08 20:11:35 +0800

    feat: added code for header component

---
+++ components/header.php
@@ -0,0 +1,21 @@
+<?php include_once __DIR__ . '/../config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title> Welcome to my <?php echo $theme; ?> Fan Page!</title>
+        <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
+    </head>
+
+    <body>
+        <header>
+            <h1>Bocchi the Rock! - Fan Showcase</h1>
+            <nav>
+                <a href="<?php echo BASE_URL; ?>index.php">Home</a>
+                <a href="<?php echo BASE_URL; ?>">Insert Here</a>
+            </nav>
+        </header>
+    </body>
+
+</html>
