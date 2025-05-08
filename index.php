76fc379db69a73a5517f7cba7bae9e9d8b7c056d
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-08 22:48:52 +0800

    feat: add main content structure in index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'components/header.php'; ?>
+<main>
+    <h1>Welcome to the <?php echo $theme; ?> Fan Page!</h1>
+    <p>This simple PHP site uses declarations, conditional statements and looping statements to showcase the basics of
+        PHP in the style of <?php echo $theme; ?>.</p>
+    <?php
+    $featuredCharacters = ['Hitori Gotou', 'Nijika Ijichi', 'Kita Ikuyo', 'Ryou Yamada'];
+
+    ?>
+</main>
