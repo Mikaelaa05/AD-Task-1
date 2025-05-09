2905367940c11d90511ecb1a589f63cd928a80f0
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-09 21:48:21 +0800

    refactor: recoded header.php

--- components/header.php
+++ components/header.php
@@ -1,19 +1,22 @@
-<?php include_once __DIR__ . '/../config.php'; ?>
+<?php include_once __DIR__ . '/utils/config.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
     <head>
-        <meta charset="UTF-8">
-        <title> Welcome to my <?php echo $theme; ?> Fan Page!</title>
+        <title><?php echo $theme; ?> Fan Page</title>
         <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
     </head>
 
     <body>
         <header>
-            <h1>Bocchi the Rock! - Fan Showcase</h1>
+            <h1><?php echo $theme; ?></h1>
             <nav>
-                <a href="<?php echo BASE_URL; ?>index.php">Home</a>
-                <a href="<?php echo BASE_URL; ?>">Insert Here</a>
+                <ul>
+                    <li><a href="<?php echo BASE_URL; ?>index.php">Home</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>page/characters.php">Characters</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>page/episodes.php">Episodes</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>page/about.php">About</a></li>
+                </ul>
             </nav>
         </header>
     </body>
