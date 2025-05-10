11630fdd2b097c60ab6f1d9e57a25f628ec90b35
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-10 22:59:40 +0800

    feat: add characters.php page with character listings

---
+++ page/characters.php
@@ -0,0 +1,11 @@
+<?php include 'components/header.php'; ?>
+<section>
+    <h2>Characters</h2>
+    <p>Meet the amazing characters from <?php echo $theme; ?>!</p>
+    <ul>
+        <li><a href="hitori.php">Gotoh Hitori</a>i</li>
+        <li><a href="kita.php">Ikuyo Kita</a></li>
+        <li><a href="nijika.php">Ichiji Nijika</a></li>
+        <li><a href="ryo.php">Yamada Ryo</a></li>
+    </ul>
+    <?php include 'components/footer.php'; ?>
