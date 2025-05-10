7db441082fd298ece9ff7fcabdc708051fa514ce
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 00:08:28 +0800

    feat: create kita.php page with content and structure

---
+++ .php_tmp/kita.php
@@ -0,0 +1,10 @@
+<?php include_once '../components/header.php'; ?>
+
+<div class="card">
+    <h2>Ikuyo Kita</h2>
+    <img src="assets\img\kita_1.jpg" alt="Ikuyo Kita" width="300" height="400">
+    <p>Ikuyo Kita is a member of the Kessoku Band and plays the guitar. She is known for her cheerful personality and
+        love for music.</p>
+</div>
+
+<?php include_once '../components/footer.php'; ?>
