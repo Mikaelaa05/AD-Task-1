9bd910e3956bdb3c94bf375b7ab7d3ef7db4bfb0
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-10 23:05:56 +0800

    feat: add hitori.php page with character details

---
+++ page/hitori.php
@@ -0,0 +1,8 @@
+<?php include 'components/header.php'; ?>
+
+<h2>Gotoh Hitori</h2>
+<img src="assets\img\gotoh_hitori_1.jpg" alt="Gotoh Hitori" width="300" height="400">
+<p>Gotoh Hitori, also known as Bocchi-chan, is the main character of the <?php echo $theme ?>. She is a shy and
+    introverted</p>
+
+<?php include 'components/footer.php'; ?>
