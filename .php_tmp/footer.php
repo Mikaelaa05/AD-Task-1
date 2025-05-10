637f89456f0aeee3971f9cfa0d45fe912b3e14b0
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 00:16:04 +0800

    refactor: remove unused header and footer components from temporary files

--- .php_tmp/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer>
-    <p>&copy; 2025 <?php echo $theme; ?> Fan Page</p>
-</footer>
-</body>
-
-</html>
--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,3 @@
-</main>
 <footer>
     <p>&copy; 2025 <?php echo $theme; ?> Fan Page</p>
 </footer>
