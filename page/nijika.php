668ace062f943ef1f6031d6a18943cc3f0bf2fa7
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 00:21:33 +0800

    feat: add nijika.php page with content and structure

---
+++ page/nijika.php
@@ -0,0 +1,10 @@
+<?php include_once '/../components/header.php'; ?>
+
+<div class="card">
+    <h2>Ichiji Nijika</h2>
+    <img src="assets\img\nijika_1.jpg" alt="Nijika Shinozaki" width="300" height="400">
+    <p>Ichiji Nijika is the drummer of the Kessoku Band. She is known for her energetic personality and love for
+        music.</p>
+</div>
+
+<?php include_once '/../components/footer.php'; ?>
