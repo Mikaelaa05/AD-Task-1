2beae5c44e33f04b1ef2bcca6f6c46284eefacdd
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 21:05:56 +0800

    fix: improve protocol detection in config.php

--- utils/config.php
+++ utils/config.php
@@ -1,5 +1,5 @@
 <?php
-$protocol = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
 $host = $_SERVER['HTTP_HOST'];
 $folder = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
 define('BASE_URL', $protocol . $host . $folder . '/');
