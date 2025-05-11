f6ed1469961ffbde9f3bb63393c29f1c21808975
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 20:34:31 +0800

    fix: updated style.css code to hopefully work

--- utils/config.php
+++ utils/config.php
@@ -1,4 +1,7 @@
 <?php
-define('BASE_URL', '/AD-Task-1/');
+$protocol = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
+$host = $_SERVER['HTTPS_HOST'];
+$folder = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+define('BASE_URL', $protocol . $host . $folder . '/');
 $theme = 'Bocchi the Rock!';
 ?>
