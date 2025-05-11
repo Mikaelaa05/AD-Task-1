cd775dce396f9c9508dae5888146b3cd2513adb8
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 20:56:39 +0800

    fix: update image source path and alt text for Ichiji Nijika

--- page/nijika.php
+++ page/nijika.php
@@ -2,7 +2,7 @@
 
 <div class="card">
     <h2>Ichiji Nijika</h2>
-    <img src="assets\img\nijika_1.jpg" alt="Nijika Shinozaki" width="300" height="400">
+    <img src="<?php echo BASE_URL; ?>assets/img/ichiji_nijika_1.jpg" alt="Ichiji Nijika" width="400" height="400">
     <p>Ichiji Nijika is the drummer of the Kessoku Band. She is known for her energetic personality and love for
         music.</p>
 </div>
