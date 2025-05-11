de0b7985cb7f057b813d536cfa523a16eb0d676a
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 20:57:40 +0800

    fix: update image source path and alt text for Yamada Ryo

--- page/ryo.php
+++ page/ryo.php
@@ -2,7 +2,7 @@
 
 <div class="card">
     <h2>Yamada Ryo</h2>
-    <img src="assets\img\nijika_1.jpg" alt="Nijika Shinozaki" width="300" height="400">
+    <img src="<?php echo BASE_URL; ?>assets/img/yamada_ryo_1.jpg" alt="Yamada Ryo" width="400" height="400">
     <p>Yamada Ryo is the band's bassist. She is cool and mysterious with a quirky attitude.</p>
 </div>
 
