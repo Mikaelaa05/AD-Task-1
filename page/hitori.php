db89144b3b3fb5b36238e440c584bc3ce0e8aeed
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-11 20:47:59 +0800

    refactor: changed width

--- page/hitori.php
+++ page/hitori.php
@@ -2,7 +2,7 @@
 
 <div class="card">
     <h2>Gotoh Hitori</h2>
-    <img src="<?php echo BASE_URL; ?>assets/img/gotoh_hitori_1.jpg" alt="Gotoh Hitori" width="300" height="400">
+    <img src="<?php echo BASE_URL; ?>assets/img/gotoh_hitori_1.jpg" alt="Gotoh Hitori" width="400" height="400">
     <p>Gotoh Hitori, also known as Bocchi-chan, is the main character of the <?php echo $theme ?>. She is a shy and
         introverted</p>
 </div>
