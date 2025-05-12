1c5b74e1ba338b4a9285a815a2bfc17047f1a2e1
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-12 14:25:49 +0800

    feat: update footer with additional information and contact link

--- .php_tmp/footer.php
+++ .php_tmp/footer.php
@@ -1,5 +1,9 @@
 <footer>
     <p>&copy; 2025 <?php echo $theme; ?> Fan Page</p>
+    <p>THIS FAN PAGE IS UNOFFICIAL AND NOT AFFILIATED WITH BOCCHI THE ROCK!, ANIPLEX AND SONY MUSIC ENTERTAINMENT.</p>
+    <p>
+        < href="<?php echo BASE_URL; ?>page/contact.php" Contact Us</a>
+    </p>
 </footer>
 </body>
 
