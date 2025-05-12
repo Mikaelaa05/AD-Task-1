0992af53ddc6640631c2be06665e86fcfe7b6efb
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-12 22:11:50 +0800

    refactor: restructure character list to use figure and figcaption for better semantics

--- page/characters.php
+++ page/characters.php
@@ -3,10 +3,21 @@
 <div class="card">
     <h2>Meet Kessoku Band!</h2>
     <div class="character-list">
-        <div class="character-item"><a href="hitori.php">Gotoh Hitori</a></div>
-        <div class="character-item"><a href="kita.php">Ikuyo Kita</a></div>
-        <div class="character-item"><a href="nijika.php">Ichiji Nijika</a></div>
-        <div class="character-item"><a href="ryo.php">Yamada Ryo</a></div>
+        <figure class="character-item">
+            <img src="<?php echo BASE_URL; ?>assets/img/gotoh_hitori_1.jpg" alt="Gotoh Hitori">
+            <figcaption><a href="hitori.php">Gotoh Hitori</a></figcaption>
+        </figure>
+        <figure class="character-item">
+            <img src="<?php echo BASE_URL; ?>assets/img/ikuyo_kita_1.jpg" alt="Ikuyo Kita">
+            <figcaption><a href="kita.php">Ikuyo Kita</a></figcaption>
+        </figure>
+        <figure class="character-item">
+            <img src="<?php echo BASE_URL; ?>assets/img/ichiji_nijika_1.jpg" alt="Ichiji Nijika">
+            <figcaption><a href="nijika.php">Ichiji Nijika</a></figcaption>
+            <figure class="character-item">
+                <img src="<?php echo BASE_URL; ?>assets/img/yamada_ryo_1.jpg" alt="Yamada Ryo">
+                <figcaption><a href="ryo.php">Yamada Ryo</a></figcaption>
+            </figure>
     </div>
 </div>
 
