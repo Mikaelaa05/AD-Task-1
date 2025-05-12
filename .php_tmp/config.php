6721e37b1a78b30cf0214e9c048afc2bd27093e8
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-12 14:27:46 +0800

    fix: fixed Contact Us hyperlink on footer.php

--- .php_tmp/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('BASE_URL', '/'); // Simplified for local development with FiveServer
-$theme = 'Bocchi the Rock!';
-?>
--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,7 @@
 <footer>
     <p>&copy; 2025 <?php echo $theme; ?> Fan Page</p>
     <p>THIS FAN PAGE IS UNOFFICIAL AND NOT AFFILIATED WITH BOCCHI THE ROCK!, ANIPLEX AND SONY MUSIC ENTERTAINMENT.</p>
-    <p>
-        < href="<?php echo BASE_URL; ?>page/contact.php" Contact Us</a>
-    </p>
+    <p><a href="<?php echo BASE_URL; ?>page/contact.php">Contact Us</a></p>
 </footer>
 </body>
 
