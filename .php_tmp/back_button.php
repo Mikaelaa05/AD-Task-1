0ec15059b35be4d8b0dd618cf474a3a99e86a141
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-12 14:59:33 +0800

    feat: add back button component for navigation

---
+++ .php_tmp/back_button.php
@@ -0,0 +1,5 @@
+<?php
+<!-- filepath: d:/School Files/FEU Tech School Files/FEU Tech/2nd Year/3rd Sem/App Dev LEC/Repos/AD-Task-1/components/backbutton.php -->
+
+<div class = "back-button">
+    <a href="<?php echo BASE_URL; ?>index.php" class="btn btn-primary">Back</a>
