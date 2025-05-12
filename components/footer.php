b6396621b5c2c110637eb75a230d2da0ee21b9f6
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-12 15:36:56 +0800

    chore: updated credits in components/footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,7 @@
 <footer>
     <p>&copy; 2025 <?php echo $theme; ?> Fan Page</p>
-    <p>THIS FAN PAGE IS UNOFFICIAL AND NOT AFFILIATED WITH BOCCHI THE ROCK!, ANIPLEX AND SONY MUSIC ENTERTAINMENT.</p>
+    <p>THIS FAN PAGE IS UNOFFICIAL AND NOT AFFILIATED WITH BOCCHI THE ROCK!, CLOVERWORKS, ANIPLEX AND SONY MUSIC
+        ENTERTAINMENT.</p>
     <p>Made by MikaTheRock</p>
     <p><a href="<?php echo BASE_URL; ?>page/contact.php">Contact us here!</a></p>
 </footer>
