8f411e8b8fccee33177f416acdf03a4fad63bd09
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-12 17:34:31 +0800

    refactor: updated code for page/assets/css/about.css and page/about.php to look much better

--- page/about.php
+++ page/about.php
@@ -1,6 +1,6 @@
 <?php include '../components/header.php'; ?>
 
-<div class="card">
+<div class="card about-section">
     <h2>About <?php echo $theme ?></h2>
     <p><?php echo $theme ?> is a Japanese anime series that follows the story of Gotoh Hitori, a shy and introverted
         girl who dreams of becoming a rock star. She joins the Kessoku Band and embarks on a journey of self-discovery,
@@ -8,24 +8,22 @@
 
     <div class="about-images">
         <figure>
-            <img src="<?php echo BASE_URL; ?>assets/img/Hamaji Aki 1.jpg" alt="Hamaji Aki 1" width="225" height="350">
+            <img src="<?php echo BASE_URL; ?>assets/img/Hamaji Aki 1.jpg" alt="Hamaji Aki 1">
             <figcaption>Hamaji Aki</figcaption>
             <p>Hamaji Aki is the mangaka and creator of <?php echo $theme ?>.</p>
         </figure>
         <figure>
-            <img src="<?php echo BASE_URL; ?>assets/img/Kerorira 1.jpg" alt="Kerorira 1" width="400" height="400">
+            <img src="<?php echo BASE_URL; ?>assets/img/Kerorira 1.jpg" alt="Kerorira 1">
             <figcaption>Kerorira</figcaption>
             <p>Hamaji Aki is the mangaka and creator of <?php echo $theme ?>.</p>
         </figure>
         <figure>
-            <img src="<?php echo BASE_URL; ?>assets/img/cloverworkslogo.jpg" alt="CloverWorks Logo" width="400"
-                height="400">
+            <img src="<?php echo BASE_URL; ?>assets/img/cloverworkslogo.jpg" alt="CloverWorks Logo">
             <figcaption>CloverWorks</figcaption>
             <p>Hamaji Aki is the mangaka and creator of <?php echo $theme ?>.</p>
         </figure>
         <figure>
-            <img src="<?php echo BASE_URL; ?>assets/img/Kessoku Band Jump Shot NEW.jpg" alt="Kessoku Band Jumpshot"
-                width="400" height="400">
+            <img src="<?php echo BASE_URL; ?>assets/img/Kessoku Band Jump Shot NEW.jpg" alt="Kessoku Band Jumpshot">
             <figcaption>The anime</figcaption>
             <p>Hamaji Aki is the mangaka and creator of <?php echo $theme ?>.</p>
         </figure>
